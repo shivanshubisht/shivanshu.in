1cc1b714eaa3e1dbfbb698525ff370d8eaf8b4b3
shivanshubisht <77038395+shivanshubisht@users.noreply.github.com>
2022-12-29 16:08:02 +0530

    Add files via upload

---
+++ app/head.tsx
@@ -0,0 +1,14 @@
+export default function Head() {
+  return (
+    <>
+      <title>Shivanshu&apos;s Personal Website</title>
+      <meta content='width=device-width, initial-scale=1' name='viewport' />
+      <meta
+        name='description'
+        content="Shivanshu's Personal Website, Front End Developer in Next JS"
+      />
+      <link rel='icon' href='/favicon.ico' />
+      <script src='https://www.goat1000.com/tagcanvas.min.js' async></script>
+    </>
+  );
+}

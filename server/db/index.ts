f51ed6bfac85fce972aad7ff8da6076bf1966084
shivanshu <bishtshivanshu@gmail.com>
2024-04-22 01:28:55 +0530

    v2 init

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,13 +1,13 @@
-import { env } from '@/env'
-import { Pool } from '@neondatabase/serverless'
-import { drizzle } from 'drizzle-orm/neon-serverless'
+import { env } from "@/env";
+import { Pool } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
 
 const globalForDb = globalThis as unknown as {
-  pool: Pool | undefined
-}
+  pool: Pool | undefined;
+};
 
 const pool =
-  globalForDb.pool ?? new Pool({ connectionString: env.DATABASE_URL })
-if (process.env.NODE_ENV !== 'production') globalForDb.pool = pool
+  globalForDb.pool ?? new Pool({ connectionString: env.DATABASE_URL });
+if (process.env.NODE_ENV !== "production") globalForDb.pool = pool;
 
-export const db = drizzle(pool)
+export const db = drizzle(pool);
--- server/db/schema.ts
+++ server/db/schema.ts
@@ -1,20 +1,20 @@
-import { pgTable, text, timestamp } from 'drizzle-orm/pg-core'
-import { nanoid } from 'nanoid'
+import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { nanoid } from "nanoid";
 
-export const contacts = pgTable('Contact', {
+export const contacts = pgTable("Contact", {
   // id: serial('id').primaryKey(),
   // id: text('id').primaryKey().default(sql`uuid_generate_v4()`),
-  id: text('id')
+  id: text("id")
     .primaryKey()
     .$defaultFn(() => nanoid())
     .notNull(),
-  name: text('name'),
-  email: text('email'),
-  message: text('message'),
+  name: text("name"),
+  email: text("email"),
+  message: text("message"),
   //   createdAt: timestamp('created_at')
   //     .default(sql`CURRENT_TIMESTAMP`)
   //     .notNull(),
-  createdAt: timestamp('createdAt', { precision: 3, mode: 'string' })
+  createdAt: timestamp("createdAt", { precision: 3, mode: "string" })
     .defaultNow()
     .notNull(),
-})
+});
